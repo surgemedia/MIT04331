1aac93abbc9fb7e82174861554d3aed6559beec9
hectornucv <hectornucv@gmail.com>
2015-10-01 13:18:49 +1000

    wordpress update

--- wp-config.php
+++ wp-config.php
@@ -3,9 +3,9 @@
  * The base configurations of the WordPress.
  *
  * This file has the following configurations: MySQL settings, Table Prefix,
- * Secret Keys, WordPress Language, and ABSPATH. You can find more information
- * by visiting {@link http://codex.wordpress.org/Editing_wp-config.php Editing
- * wp-config.php} Codex page. You can get the MySQL settings from your web host.
+ * Secret Keys, and ABSPATH. You can find more information by visiting
+ * {@link https://codex.wordpress.org/Editing_wp-config.php Editing wp-config.php}
+ * Codex page. You can get the MySQL settings from your web host.
  *
  * This file is used by the wp-config.php creation script during the
  * installation. You don't have to use the web site, you can just copy this file
@@ -13,41 +13,25 @@
  *
  * @package WordPress
  */
- 
-/*====================================
-=            LOCAL CONFIG            =
-====================================*/
 
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'mit04331_jollyhelp38');
 
-// Include local configuration
-if (file_exists(dirname(__FILE__) . '/local-config.php')) {
-	include(dirname(__FILE__) . '/local-config.php');
-}
+/** MySQL database username */
+define('DB_USER', 'mit04331_7uMWxEN');
 
+/** MySQL database password */
+define('DB_PASSWORD', 'zh*a9aJ2zVsWAa56Cd!+');
 
-// Global DB config
-if (!defined('DB_NAME')) {
-	define('DB_NAME', 'mit04331_jollyhelp38');
-}
-if (!defined('DB_USER')) {
-	define('DB_USER', 'mit04331_7uMWxEN');
-}
-if (!defined('DB_PASSWORD')) {
-	define('DB_PASSWORD', 'zh*a9aJ2zVsWAa56Cd!+');
-}
-if (!defined('DB_HOST')) {
-	define('DB_HOST', 'localhost');
-}
+/** MySQL hostname */
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
-if (!defined('DB_CHARSET')) {
-	define('DB_CHARSET', 'utf8');
-}
+define('DB_CHARSET', 'utf8mb4');
 
 /** The Database Collate type. Don't change this if in doubt. */
-if (!defined('DB_COLLATE')) {
-	define('DB_COLLATE', '');
-}
+define('DB_COLLATE', '');
 
 /**#@+
  * Authentication Unique Keys and Salts.
@@ -58,14 +42,14 @@ if (!defined('DB_COLLATE')) {
  *
  * @since 2.6.0
  */
-define('AUTH_KEY',         '<HnG);:814i;5>UB:v|4!Xg$->Fjo1Q[XngYg+Xi*i~OWVE:QfROJ!Hb0)tkC-6>');
-define('SECURE_AUTH_KEY',  'x4Ypv_D)+YOVL6Z?;A#_Z9*W2 5u*uOPE/|l~pW*,;Txz:)a! :Y(-C| ?xVN<o5');
-define('LOGGED_IN_KEY',    '$m+qH.^?HXeb|S|%JlAX`h2D@#:$WdDsZN;8pS{Q,eDU$xU_W5gzZ_ZuGL]WkSW:');
-define('NONCE_KEY',        'kuLjwi}Z<_*Z(#|WIGcBK+;}fX(L%JU%ovLV~4]Z ko|=S~iFP+`-Rv+f8hHV*mC');
-define('AUTH_SALT',        'uJE+wazbd%Z8<t_>s5*&f_j|7}{S@;vH ph~#B;KT Kb /=}ebu+%mn>dZ+`q^E=');
-define('SECURE_AUTH_SALT', '3dZMP:Vc57o^I0._>nQ1frBU^xF!$+*jjCrUkp |-x(_Z(;Q-/cq?re}%7!a@C9Q');
-define('LOGGED_IN_SALT',   'c8,JeyPW$1p|!Rmg.*Gb$-`QJ3Dyq=(W=T.|WI7%Z&$-OTFSPjvyoA![TGmTku>w');
-define('NONCE_SALT',       't@!D;}s-|7$Y`4f#+{[aOmZC)&|zJ$#|;|hQ: HTX~*YLoA.R{U54B[U-o6}mCHn');
+define('AUTH_KEY',         '1?a+dvy}Nc <`1 xLp3Kr&rb%R87hRDgEw>XfZ+cG-Z;_|^ssW@F!D%sxUN9znWF');
+define('SECURE_AUTH_KEY',  'MU5ARL&V(/>)@ hk9OByX/T=m{N4R~|o@/S|@r7!Y<lAY,v9T]2L@J`Zvva<cFzE');
+define('LOGGED_IN_KEY',    'ahiEG|l/cXnCG<V,4.L#cIb6t@rMV_]4ktdpY_B|^C_G/6 Tuc8BpV~,[)s^5Xoj');
+define('NONCE_KEY',        'WYX&Hg|)?Ssr}jYC]y@/k)P^A<dp.c@=C?FnU|Y#_D7vIuS<aPV=Zca)-]])b/im');
+define('AUTH_SALT',        'BaFZO}E4$qPLH5;d{aGU:)mw4$9t8}gACt*f_aooy}HP1t.RH{5u+|I:tKp{s562');
+define('SECURE_AUTH_SALT', 'B/k`y4q!<ht:uh&KP=]S/,1SUF*uA-G6#EJ-TsN6CY+Y4NK/M-LTYi?mFdE|e,vq');
+define('LOGGED_IN_SALT',   '|xv%M6?HRiI/&sjv*C,Wimk%6XFPvF9-&&)|CkB&2hT59T}l]RjVHtMfX,K(sym[');
+define('NONCE_SALT',       'Y=C;|dO&6dw9EZ.X6`-<C.,QK6l&&Q=2WZv0>AJ{26,CW} B(ve #!QN%_Aq|Q=0');
 
 /**#@-*/
 
@@ -78,29 +62,13 @@ define('NONCE_SALT',       't@!D;}s-|7$Y`4f#+{[aOmZC)&|zJ$#|;|hQ: HTX~*YLoA.R{U5
 $table_prefix  = 'wp_';
 
 /**
- * WordPress Localized Language, defaults to English.
- *
- * Change this to localize WordPress. A corresponding MO file for the chosen
- * language must be installed to wp-content/languages. For example, install
- * de_DE.mo to wp-content/languages and set WPLANG to 'de_DE' to enable German
- * language support.
- */
-define('WPLANG', '');
-
-
-
-/**
  * For developers: WordPress debugging mode.
  *
  * Change this to true to enable the display of notices during development.
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-if (!defined('WP_DEBUG')) {
-	define('WP_DEBUG', false);
-	define( 'SCRIPT_DEBUG', true );
-
-}
+define('WP_DEBUG', false);
 
 /* That's all, stop editing! Happy blogging. */
 
